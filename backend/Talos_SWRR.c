81f5cbdf7427b2a4272459001d7918fd0bea1851
huang-zhen <z.huang@mail.utoronto.ca>
2018-01-19 14:12:41 -0500

    Added backend

---
+++ backend/Talos_SWRR.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018 Zhen Huang
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+static char *SWRR_logfile = NULL;
+
+void SWRR_log(const char *msg) {
+	static FILE *fp = NULL;
+	if (fp == NULL)
+		fp = fopen(SWRR_logfile, "a");
+	if (fp)
+		fprintf(fp, "%s\n", msg);
+}
+
+void SWRR_exit() {
+	char buf[255];
+
+	sprintf(buf, "Exiting...%s", SWRR_entry);
+	SWRR_log(buf);
+}
+
+void SWRR_loader() {
+	int i;
+	char buf[255];
+
+	SWRR_logfile = getenv("TALOS_SWRRLOG");
+	time_t t;
+	time(&t); 
+	sprintf(buf, "%s Starting...%s", ctime(&t), SWRR_entry);
+	SWRR_log(buf);
+	if (atexit(SWRR_exit) != 0)
+		SWRR_log("Cannot register exit function!");
+	for (i = 0; i < sizeof(SWRR_flags_names)/sizeof(SWRR_flags_names[0]); i++) {
+		if (getenv(SWRR_flags_names[i])) {
+			SWRR_flags[i] = 1;
+			sprintf(buf, "%s %d", SWRR_flags_names[i], i);
+			SWRR_log(buf);
+		}
+	}
+}
+
---
+++ backend/Talos_exec.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2018 Zhen Huang
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+static char *EXEC_logfile = NULL;
+
+void EXEC_log(const char *msg) {
+	static FILE *fp = NULL;
+	if (fp == NULL)
+		fp = fopen(EXEC_logfile, "a");
+	if (fp) {
+		fprintf(fp, "%d:%s\n", getpid(), msg);
+		fflush(fp);
+	}
+}
+
+void EXEC_exit() {
+	char buf[255];
+	int i;
+
+	for (i = 0; i < sizeof(EXEC_flags)/sizeof(EXEC_flags[0]); i++) {
+		if (EXEC_flags[i]) {
+			sprintf(buf, "%s %d", EXEC_flags_names[i], i);
+			EXEC_log(buf);
+		}
+	}
+	sprintf(buf, "Exiting...%s", EXEC_entry);
+	EXEC_log(buf);
+}
+
+void EXEC_loader() {
+	char buf[255];
+
+	EXEC_logfile = getenv("TALOS_EXECLOG");
+	sprintf(buf, "Starting...%s", EXEC_entry);
+	EXEC_log(buf);
+	if (atexit(EXEC_exit) != 0)
+		EXEC_log("Cannot set exit function");
+}
+
