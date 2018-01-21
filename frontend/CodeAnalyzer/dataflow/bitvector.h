602a860c763e661c532dd560a4c3c21c83b9a60d
James Huang <z.huang@mail.utoronto.ca>
2018-01-20 20:21:04 -0500

    Rewrote to improve performance.

--- frontend/CodeAnalyzer/dataflow/bitvector.h
+++ frontend/CodeAnalyzer/dataflow/bitvector.h
@@ -5,26 +5,26 @@
 #include <string>
 
 class Bitvector {
-private:
-	std::vector<unsigned char> m_bits;
 public:
+	int *m_bits;
+	int m_size; // size of m_bits
+	int m_bsize; // size in bits
 	Bitvector(int size);
+	Bitvector(unsigned char a[], int size);
+	Bitvector(const Bitvector& bv);
 	~Bitvector();
 	int size() const;
-	void merge(Bitvector bv);
-	void diff(Bitvector bv);
-	void intersect(Bitvector bv);
+	bool get(int bit) const;
+	void set(int bit, bool val);
+	void merge(const Bitvector& bv);
+	void diff(const Bitvector& bv);
+	void intersect(const Bitvector& bv);
 	void print() const;
-	Bitvector& operator=(const Bitvector bv);
-	bool operator!=(const Bitvector bv);
-	bool operator==(const Bitvector bv);
-	bool get(int bit) const {
-		return m_bits[bit];
-	};
-	void set(int bit, bool val) {
-		m_bits[bit] = val;
-	};
+	Bitvector& operator=(const Bitvector& bv);
+	bool operator!=(const Bitvector& bv);
+	bool operator==(const Bitvector& bv);
 	std::string tostring() const;
+	unsigned char *toarray(int &size) const;
 };
 
 #endif
