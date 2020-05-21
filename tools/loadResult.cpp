bc7f3225b1380abf975e7ffaa0ac6b47886aa418
james <z.huang@mail.utoronto.ca>
2020-05-21 16:00:58 -0500

    Added the data structure to store the control dependency

--- tools/loadResult.cpp
+++ tools/loadResult.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
+#include <map>
 #include <sstream>
 #include <cstdlib>
 
@@ -18,6 +20,8 @@ void splitStr(const std::string& s, char delimiter, std::vector<std::string>& to
    }
 }
 
+std::map<int, std::list<std::string> > controlDep;
+
 bool loadControlDependency(const char *infile, const string& module, const string& function) {
 	ifstream ifs;
 	ifs.open(infile);
@@ -26,20 +30,26 @@ bool loadControlDependency(const char *infile, const string& module, const strin
 		while (getline(ifs, line)) {
 			vector<string> tokens;
 			splitStr(line, '@', tokens);
-			//for (unsigned i = 0; i < tokens.size(); i++)
-			//	cout << i << ':' << tokens[i] << '\n';
 			const string& mod = tokens[0];
 			string& func = tokens[4];
 			if (func.find("main_") == 0)
 				func = "main";
 			const string& accessType = tokens[9];
 			const string& callType = tokens[10];
-			//cout << mod << '@' << func << '@' << accessType << '@' << callType << '\n';
-			//if (mod == module && func == function && accessType == "-2" && callType == "3") {
 			if ((mod == module) && (func == function) && (accessType == "-2") && (callType == "3")) {
 				const string& line = tokens[3];
 				const string& dep = tokens[6];
 				cout << line << "<-" << dep << '\n';
+				stringstream ss(line);
+				int l;
+				ss >> l;
+				if (controlDep.find(l) == controlDep.end()) {
+					controlDep[l] = list<string>();
+					vector<string> depList;
+					splitStr(dep, ',', depList);
+					for (unsigned i = 0; i < depList.size(); i++)
+						controlDep[l].push_front(depList[i]);
+				}
 			}
 		}
 		ifs.close();
@@ -49,12 +59,22 @@ bool loadControlDependency(const char *infile, const string& module, const strin
 
 int main(int argc, const char *argv[]) {
 	if (argc < 4) {
-		cerr << "Usage: " << argv[0] << " module function analyzer.out\n";
+		cerr << "Usage: " << argv[0] << " analyzer.out module function\n";
 		exit(0);
 	}
 	const char *infile = argv[1];
 	const char *module = argv[2];
 	const char *function = argv[3];
 	loadControlDependency(infile, module, function);
+    for (map<int, list<string> >::iterator it = controlDep.begin(); it != controlDep.end(); it++) {
+			cout << it->first << ':';
+			for (list<string>::iterator iit = it->second.begin(); iit != it->second.end(); iit++) {
+				if (iit == it->second.begin())
+					cout << *iit;
+				else
+					cout << ',' << *iit;
+			}
+			cout << '\n';
+	}
 	return 0;
 }
